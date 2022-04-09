b50c6a5fb844efbc55fd0417186dc9030a3e2bf1
Jeonminji <minjia13@gmail.com>
2022-04-09 22:25:23 +0900

    spring 업로드

---
+++ src/main/java/com/example/flatB/FlatBApplication.java
@@ -0,0 +1,13 @@
+package com.example.flatB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlatBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlatBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/flatB/FlatBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flatB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlatBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
