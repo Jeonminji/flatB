8e13d6903b7c6ffcc5563b8725a8e30cb93104bc
이민주 <100738702+mjlee-00@users.noreply.github.com>
2022-05-10 20:25:55 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='navbar'>
+        <div>플랫B</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
