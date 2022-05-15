2d589601fceaad094188b0ea811cc99e1be06ee1
inhui9674 <2000inhi@naver.com>
2022-05-16 00:00:15 +0900

    PROJ-40 Feat: 게시글 response 관련 메시지 추가

--- src/main/java/com/example/flatB/common/ResponseMessage.java
+++ src/main/java/com/example/flatB/common/ResponseMessage.java
@@ -14,6 +14,16 @@ public class ResponseMessage {
     public static final String NOT_DUPLICATED_NICKNAME = "닉네임 사용 가능";
     public static final String DUPLICATED_NICKNAME = "닉네임 중복";
 
+    //게시글 관련
+    public static final String POST_CREATED = "게시글 작성 성공";
+    public static final String POST_NOT_CREATED = "게시글 작성 실패";
+    public static final String POST_READ = "게시글 조회 성공";
+    public static final String POST_READ_FAIL = "게시글 조회 실패";
+    public static final String POST_UPDATE = "게시글 수정 성공";
+    public static final String POST_UPDATE_FAIL = "게시글 수정 실패";
+    public static final String POST_DELETE = "게시글 삭제 성공";
+    public static final String POST_DELETE_FAIL = "게시글 삭제 실패";
+
     public static final String INTERNAL_SERVER_ERROR = "서버 내부 에러";
     public static final String DB_ERROR = "데이터베이스 에러";
 }
