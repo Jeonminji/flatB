af3463a839377d3bd03161089570c192c284aac2
Jeonminji <minjia13@gmail.com>
2022-05-19 23:35:22 +0900

    Feat: report Entity 클래스 추가

---
+++ src/main/java/com/example/flatB/domain/entity/ReportEntity.java
@@ -0,0 +1,50 @@
+package com.example.flatB.domain.entity;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Builder
+@Entity
+@Table(name = "declaration")
+public class ReportEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer boardNo;
+
+    @Column(length = 15, nullable = false)
+    private String nickname;
+
+    @Column(length = 100, nullable = false)
+    private String note;
+
+    @Column(length = 10, nullable = false)
+    private String type;
+
+    @Column(length = 200, nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "writer_id")
+    private UserEntity user;
+
+    @CreationTimestamp
+    @DateTimeFormat(pattern = "dd.MM.yyyy hh:mm")
+    private LocalDateTime date;
+
+//    @Builder
+//    public BoardEntity(Integer boardNo, String nickname, String note, String type, String content, String writeId) {
+//        this.boardNo = boardNo;
+//        this.nickname = nickname;
+//        this.note = note;
+//        this.type = type;
+//        this.content = content;
+//    }
+}
