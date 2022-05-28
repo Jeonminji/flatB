93b829a50faf6f034dbc0ad5e7b0e0c30f3cc484
Jeonminji <minjia13@gmail.com>
2022-05-28 19:58:13 +0900

    PROJ-42 Feat: recruitment save Dto 추가

---
+++ src/main/java/com/example/flatB/domain/dto/RecruitmentSaveDto.java
@@ -0,0 +1,41 @@
+package com.example.flatB.domain.dto;
+
+import com.example.flatB.domain.entity.RecruitmentEntity;
+import com.example.flatB.domain.entity.UserEntity;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class RecruitmentSaveDto { //저장
+    private String title;
+    private String content;
+    private String platformname;
+    private int totalcount;
+    private int currentcount;
+    private String usedateStart;
+    private String usedateEnd;
+    private String contact;
+    private UserEntity userEntity;
+    private LocalDateTime regdate;
+
+    public RecruitmentEntity toEntity() {
+        RecruitmentEntity recruitmentEntity = RecruitmentEntity.builder()
+                .title(title)
+                .content(content)
+                .platformname(platformname)
+                .totalcount(totalcount)
+                .currentcount(currentcount)
+                .usedateStart(usedateStart)
+                .usedateEnd(usedateEnd)
+                .contact(contact)
+                .userEntity(userEntity)
+                .regdate(LocalDateTime.now())
+                .build();
+        return recruitmentEntity;
+    }
+}
