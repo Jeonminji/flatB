5ea248e05c4c57b5ffe6da615012270c4e240cf4
Jeonminji <minjia13@gmail.com>
2022-05-28 19:58:55 +0900

    PROJ-42 Feat: recruitment update Dto 추가

---
+++ src/main/java/com/example/flatB/domain/dto/RecruitmentUpdateDto.java
@@ -0,0 +1,49 @@
+package com.example.flatB.domain.dto;
+
+import com.example.flatB.domain.entity.RecruitmentEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class RecruitmentUpdateDto { //수정
+    private String title;
+    private String content;
+    private String platformname;
+    private int totalcount;
+    private int currentcount;
+    private String usedateStart;
+    private String usedateEnd;
+    private String contact;
+
+    @Builder
+    public RecruitmentUpdateDto(String title, String content, String platformname, int totalcount, int currentcount,
+                                String usedateStart, String usedateEnd, String contact) {
+        this.title = title;
+        this.content = content;
+        this.platformname = platformname;
+        this.totalcount = totalcount;
+        this.currentcount = currentcount;
+        this.usedateStart = usedateStart;
+        this.usedateEnd = usedateEnd;
+        this.contact = contact;
+    }
+
+    public RecruitmentUpdateDto(RecruitmentEntity recruitmentEntity) {
+        this.title = recruitmentEntity.getTitle();
+        this.content = recruitmentEntity.getContent();
+        this.platformname = recruitmentEntity.getPlatformname();
+        this.totalcount = recruitmentEntity.getTotalcount();
+        this.currentcount = recruitmentEntity.getCurrentcount();
+        this.usedateStart = recruitmentEntity.getUsedateStart();
+        this.usedateEnd = recruitmentEntity.getUsedateEnd();
+        this.contact = recruitmentEntity.getContact();
+    }
+
+    public static RecruitmentUpdateDto ofEntity(RecruitmentEntity recruitmentEntity) {
+        return new RecruitmentUpdateDto(recruitmentEntity);
+    }
+}
