74721db403763c8d63f69c912dbf95cdd331dafd
hyunjungjeon <sksznddl574@gamil.com>
2022-06-15 09:13:19 +0900

    feat: 신고페이지 라우터 추가

--- Front/flatb_front/src/App.js
+++ Front/flatb_front/src/App.js
@@ -1,16 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Register from './pages/registerPage';
-
+import Declaration from './pages/declarationPage';
 import './App.css';
+
 function App() {
   return (
     <div className="App">
-
        <BrowserRouter>   
         <Routes> 
-            <Route  path="/" ></Route>
+            <Route  path="/"></Route>
             <Route  path="/register" element={<Register />}></Route>
-  
+            <Route  path="/report" element={<Declaration />}></Route>
           </Routes> 
       </BrowserRouter>
     </div>
