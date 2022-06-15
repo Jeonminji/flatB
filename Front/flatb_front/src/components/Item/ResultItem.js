dcd2090b7887199c1a185e384f35729ab3034f95
hyunjungjeon <sksznddl574@gamil.com>
2022-06-15 10:26:35 +0900

    PROJ-48 feat: 추천 테스트 결과 item 서버 정보 get 추가

--- Front/flatb_front/src/components/Item/ResultItem.js
+++ Front/flatb_front/src/components/Item/ResultItem.js
@@ -3,7 +3,40 @@ import axios from 'axios';
 import "./ResultItem.css"
 
 const ResultItem = (props) => {
+    const{platformName, recommend}=props;
+    const [resultImg, setResultImg]=useState("");
+    const [platform, setplatform] = useState("");
+    console.log(platformName.toUpperCase());
 
+    useEffect(() => {
+
+        if(recommend ==="OTT"){
+            axios({
+                method: "get",
+                url: "/ottTest/"+platformName,
+                responseType: "json"
+            }).then((res)=>{
+                console.log(res.data.data.ottName.length);
+                setResultImg(res.data.data.ottLogo);
+                setplatform(res.data.data.ottName);
+                
+            });
+        }
+        else if(recommend === "Music"){
+            axios({
+                        method: "GET",
+                        url: '/musicTest/'+platformName,
+                        responseType: "json"
+                        })
+                        .then((res) => {
+                            console.log(res.data.data.ottLogo);
+                            setResultImg(res.data.data.ottLogo);
+                            setplatform(res.data.data.ottName);
+                            
+                        })
+        }
+              
+    },[])
 
     return (
         <div className="recommendPlatform">
