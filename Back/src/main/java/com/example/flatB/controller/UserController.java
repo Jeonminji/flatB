9ba32d69f93d4a925b5404d373cc50e5a0f7ec75
Jeonminji <minjia13@gmail.com>
2022-06-15 17:17:54 +0900

    PROJ-22 Refactor: 로그인 코드 재작성

--- Back/src/main/java/com/example/flatB/controller/UserController.java
+++ Back/src/main/java/com/example/flatB/controller/UserController.java
@@ -26,61 +26,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class UserController {
   private final UserService userService;
 
-    // 메인 페이지
-    @GetMapping("/")
-    public String index() {
-        return "/index";
-    }
-
-    // 회원가입 페이지
-//    @GetMapping("/user/signup")
-//    public String dispSignup() {
-//        return "/signup";
-//    }
-
-    // 회원가입 처리
-//    @PostMapping("/user/signup")
-//    public String execSignup(MemberDto memberDto) {
-//        memberService.joinUser(memberDto);
+    //로그인
+//    @PostMapping("/user/login")
+//    public ResponseEntity login(@RequestParam("userId") String userId,
+//                                @RequestParam("password") String password, Errors errors) {
+//        //유효성 검사 추가
 //
-//        return "redirect:/user/login";
+//
+//        return new ResponseEntity(HttpStatus.BAD_REQUEST);
 //    }
-
-    // 로그인 페이지
-    @GetMapping("/user/login")
-    public String dispLogin() {
-        return "/login";
-    }
-
-    // 로그인 결과 페이지
-//    @GetMapping("/user/login/result")
-//    public String dispLoginResult() {
-//        return "/loginSuccess";
-//    }
-
-    // 로그아웃 결과 페이지
-    @GetMapping("/user/logout/result")
-    public String dispLogout() {
-        return "/";
-    }
-
-    // 접근 거부 페이지
-    @GetMapping("/user/denied")
-    public String dispDenied() {
-        return "/denied";
-    }
-
-    // 내 정보 페이지
-    @GetMapping("/user/info")
-    public String dispMyInfo() {
-        return "/myinfo";
-    }
-
-    // 어드민 페이지
-    @GetMapping("/admin")
-    public String dispAdmin() {
-        return "/admin";
-    }
   
    //회원가입
     @PostMapping("/user/signup")
