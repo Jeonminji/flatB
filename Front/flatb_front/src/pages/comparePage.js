2629e2626ac476a65e9459d3e85327f464e488ad
hyunjungjeon <sksznddl574@gmail.com>
2022-06-19 05:09:40 +0900

    PROJ-38 feat: 비교 페이지 정보 서버 get  추가

--- Front/flatb_front/src/pages/comparePage.js
+++ Front/flatb_front/src/pages/comparePage.js
@@ -1,5 +1,8 @@
-import React from 'react'; 
+import React,{useEffect, useState} from 'react'; 
+import axios from 'axios';
 import Header from '../components/Header/Header';
+import CompareOttItem from '../components/Item/CompareOttItem'
+import CompareMusicItem from '../components/Item/CompareMusicItem'
 import netfliximg from '../img/netflix_logo.jpg'
 import watchaimg from '../img/watcha_logo.jpg'
 import wavveimg from '../img/wavve_logo.jpg'
@@ -15,7 +18,59 @@ import genieimg from '../img/genie_logo.jpg'
 import "./comparePage.css" 
 
 const ComparePage=(props) =>{ 
+    const [netflix,setNetflix]=useState({items:[]});
+    const [watcha,setWatcha]=useState({items:[]});
+    const [wavve,setWavve]=useState({items:[]});
+    const [disney,setDisney]=useState({items:[]});
+    const [tving,setTving]=useState({items:[]});
+    const [laftel,setLaftel]=useState({items:[]});
+    const [melon,setMelon]=useState({items:[]});
+    const [spotify,setSpotify]=useState({items:[]});
+    const [flo,setFlo]=useState({items:[]});
+    const [genie,setGenie]=useState({items:[]});
+    const [youtube,setYoutube]=useState({items:[]});
+    const [bugs,setBugs]=useState({items:[]});
 
+
+    useEffect(() => {
+        axios({method: "GET", url: '/compareOtt/netflix', responseType: "json" })
+            .then((res) => { setNetflix({items:res.data.data}); });
+
+        axios({method: "GET", url: '/compareOtt/watcha', responseType: "json" })
+            .then((res) => { setWatcha({items:res.data.data}); });
+        
+        axios({method: "GET", url: '/compareOtt/wavve', responseType: "json" })
+            .then((res) => { setWavve({items:res.data.data}); });
+
+        axios({method: "GET", url: '/compareOtt/tving', responseType: "json" })
+            .then((res) => { setTving({items:res.data.data}); });
+        
+        axios({method: "GET", url: '/compareOtt/disneyplus', responseType: "json" })
+            .then((res) => { setDisney({items:res.data.data}); });
+        
+        axios({method: "GET", url: '/compareOtt/laftel', responseType: "json" })
+            .then((res) => { setLaftel({items:res.data.data}); });
+        
+        axios({method: "GET", url: '/compareMusic/melon', responseType: "json" })
+            .then((res) => { setMelon({items:res.data.data}); });
+
+        axios({method: "GET", url: '/compareMusic/spotify', responseType: "json" })
+            .then((res) => { setSpotify({items:res.data.data}); });
+
+        axios({method: "GET", url: '/compareMusic/youtubemusic', responseType: "json" })
+            .then((res) => { setYoutube({items:res.data.data}); });
+
+        axios({method: "GET", url: '/compareMusic/flo', responseType: "json" })
+            .then((res) => { setFlo({items:res.data.data}); });
+        
+        axios({method: "GET", url: '/compareMusic/genie', responseType: "json" })
+            .then((res) => { setGenie({items:res.data.data}); });
+        
+        axios({method: "GET", url: '/compareMusic/bugs', responseType: "json" })
+            .then((res) => { setBugs({items:res.data.data}); });
+        
+             
+    },[])
 
     return ( 
         <> 
@@ -28,7 +83,7 @@ const ComparePage=(props) =>{
                         <img src={netfliximg} alt="test"/>
                             <div className="compare_contents">
                                 <div className="compare_platform">NEFLIX</div>
-                                <div className="compare_hidden"></div>
+                                <div className="compare_hidden">{netflix.items.map(item=><CompareOttItem key={item.id} item={item}/>)}</div>
                             </div>
                         </div>
                     </div>
@@ -37,7 +92,7 @@ const ComparePage=(props) =>{
                         <img src={watchaimg} alt="test"/>
                             <div className="compare_contents">
                                 <div className="compare_platform">WATCHA</div>
-                                <div className="compare_hidden"></div>
+                                <div className="compare_hidden">{watcha.items.map(item=><CompareOttItem key={item.id} item={item}/>)}</div>
                             </div>
                         </div>
                     </div>
@@ -46,7 +101,7 @@ const ComparePage=(props) =>{
                         <img src={wavveimg} alt="test"/>
                             <div className="compare_contents">
                                 <div className="compare_platform">WAVVE</div>
-                                <div className="compare_hidden"></div>
+                                <div className="compare_hidden">{wavve.items.map(item=><CompareOttItem key={item.id} item={item} />)}</div>
                             </div>
                         </div>
                     </div>
@@ -55,7 +110,7 @@ const ComparePage=(props) =>{
                         <img src={tvingimg} alt="test"/>
                             <div className="compare_contents">
                                 <div className="compare_platform">TVING</div>
-                                <div className="compare_hidden"></div>
+                                <div className="compare_hidden">{tving.items.map(item=><CompareOttItem key={item.id} item={item} />)}</div>
                             </div>
                         </div>
                     </div>
@@ -64,7 +119,7 @@ const ComparePage=(props) =>{
                         <img src={disneyimg} alt="test"/>
                             <div className="compare_contents">
                                 <div className="compare_platform">DISNEYPLUS</div>
-                                <div className="compare_hidden"></div>
+                                <div className="compare_hidden">{disney.items.map(item=><CompareOttItem key={item.id} item={item} type={"different"}/>)}</div>
                             </div>
                         </div>
                     </div>
@@ -73,7 +128,7 @@ const ComparePage=(props) =>{
                         <img src={laftelimg} alt="test"/>
                             <div className="compare_contents">
                                 <div className="compare_platform">LAFTEL</div>
-                                <div className="compare_hidden"></div>
+                                <div className="compare_hidden">{laftel.items.map(item=><CompareOttItem key={item.id} item={item} />)}</div>
                             </div>
                         </div>
                     </div>
@@ -87,7 +142,7 @@ const ComparePage=(props) =>{
                     <img src={spotifyimg} alt="test"/>
                         <div className="compare_contents">
                             <div className="compare_platform">SPOTIFY</div>
-                            <div className="compare_hidden"></div>
+                            <div className="compare_hidden">{spotify.items.map(item=><CompareMusicItem key={item.id} item={item} type={"diffrent"}/>)}</div>
                         </div>
                     </div>
                 </div>
@@ -96,7 +151,7 @@ const ComparePage=(props) =>{
                     <img src={youtubeimg} alt="test"/>
                         <div className="compare_contents">
                             <div className="compare_platform">YOUTUBEMUSIC</div>
-                            <div className="compare_hidden"></div>
+                            <div className="compare_hidden">{youtube.items.map(item=><CompareMusicItem key={item.id} item={item} type={"diffrent"}/>)}</div>
                         </div>
                     </div>
                 </div>
@@ -105,7 +160,7 @@ const ComparePage=(props) =>{
                     <img src={floimg} alt="test"/>
                         <div className="compare_contents">
                             <div className="compare_platform">FLO</div>
-                            <div className="compare_hidden"></div>
+                            <div className="compare_hidden">{flo.items.map(item=><CompareMusicItem key={item.id} item={item} />)}</div>
                         </div>
                     </div>
                 </div>
@@ -114,7 +169,7 @@ const ComparePage=(props) =>{
                     <img src={bugsimg} alt="test"/>
                         <div className="compare_contents">
                             <div className="compare_platform">BUGS</div>
-                            <div className="compare_hidden"></div>
+                            <div className="compare_hidden">{bugs.items.map(item=><CompareMusicItem key={item.id} item={item} />)}</div>
                         </div>
                     </div>
                 </div>
@@ -123,7 +178,7 @@ const ComparePage=(props) =>{
                     <img src={genieimg} alt="test"/>
                         <div className="compare_contents">
                             <div className="compare_platform">GENIE</div>
-                            <div className="compare_hidden"></div>
+                            <div className="compare_hidden">{genie.items.map(item=><CompareMusicItem key={item.id} item={item} />)}</div>
                         </div>
                     </div>
                 </div>
@@ -132,7 +187,7 @@ const ComparePage=(props) =>{
                     <img src={melonimg} alt="test"/>
                         <div className="compare_contents">
                             <div className="compare_platform">MELON</div>
-                            <div className="compare_hidden"></div>
+                            <div className="compare_hidden">{melon.items.map(item=><CompareMusicItem key={item.id} item={item} />)}</div>
                         </div>
                     </div>
                 </div>
